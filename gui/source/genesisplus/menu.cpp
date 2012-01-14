44c0b5c4edb3c3edff8a7f709e6c68de5eba6e40
Ced2911 <ced2911@gmail.com>
2012-01-14 14:59:06 +0100

    thread library is not used

--- gui/source/genesisplus/menu.cpp
+++ gui/source/genesisplus/menu.cpp
@@ -23,7 +23,7 @@
 #include "w_input.h"
 #include <xenon_soc/xenon_power.h>
 #include <debug.h>
-#include <threads/threads.h>
+//#include <threads/threads.h>
 #include <xenon_smc/xenon_smc.h>
 
 #include "genesis_settings.h"
